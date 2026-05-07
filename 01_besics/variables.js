da58c4213c463e1824fe54c5a219b9c3357b5c85
Soma Das <soma.rose2010@gmail.com>
2026-05-07 18:08:34 +0000

    Variables

---
+++ 01_besics/variables.js
@@ -0,0 +1,10 @@
+const userId = 2345
+let accountEmail = "soma@gmail.com"
+var accountPassword = 123456
+cityname = "Kolkata"
+
+/*
+
+*/
+
+console.table([userId, accountEmail, accountPassword, cityname])
