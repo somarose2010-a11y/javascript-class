55f3bfef7205eb6a4b273a4fea1b9a56ea48467a
Soma Das <soma.rose2010@gmail.com>
2026-08-25 17:23:53 +0530

    update file conversionOperation

--- 01_besics/03_conversionOperation.js
+++ 01_besics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "84abc"
 
 //console.log(typeof score);
 //console.log(typeof(score)); //methode
@@ -12,10 +12,10 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "sowmya"
 
-//let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,8 +24,47 @@ let isLoggedIn = "hitesh"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
 
 //**********************Operation**************************/
-console.log(2**3)
+//console.log(2**3)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**5);
+
+// console.log("2"+2);
+// console.log(2+"2");
+// console.log("2"+2+2);
+// console.log(2+2+"2");
+
+console.log( (2+2) * (5 % 3));
+
+let num1 = 4
+let num2 = 6
+
+let result = num1 > num2
+// console.log(result);
+
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+
+//**********************Increment and Decrement prefix and postfix**************************/
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+gameCounter--
+console.log(gameCounter);
+
+gameCounter += 10
+console.log(gameCounter);
+
+gameCounter -= 10
+console.log(gameCounter);
